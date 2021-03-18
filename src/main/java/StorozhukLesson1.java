5a311924080419e1f0a6db02add576ee00441d10
bicedonetsk <bicedonetck@gmail.com>
2021-03-18 18:06:25 +0300

    2

--- src/main/java/StorozhukLesson1.java
+++ src/main/java/StorozhukLesson1.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class StorozhukLesson1 {
+public class StorozhukLesson1 {
+    public static void main(String[] args) {
+        /* tasks 1-3
+        1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
+        2. Создать переменные всех пройденных типов данных и инициализировать их значения.
+        3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+           где a, b, c, d – аргументы этого метода, имеющие тип float.
+        */
+
+        System.out.println("Hello world!");
+    }
 }
