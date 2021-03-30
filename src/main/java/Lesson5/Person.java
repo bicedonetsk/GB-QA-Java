24155e0cfdd0f4cc6238b916786a4a97d8e4e219
bicedonetsk <bicedonetck@gmail.com>
2021-03-30 04:03:56 +0300

    1

--- src/main/java/Lesson5/Person.java
+++ src/main/java/Lesson5/Person.java
@@ -9,14 +9,6 @@ public class Person {
     private String telephone;
     private int age;
     private int salary;
-    private static final String[] surnames = {"Иванов", "Петров", "Сидров", "Смирнов", "Кузнецов", "Попов", "Морозов", "Козлов", "Фёдоров", "Тарасов"};
-    private static final String[] surnamesForMail = {"ivanov", "petrov", "sidrov", "smirnov", "kuznetcov", "popov", "morozov", "kozlov", "fedorov", "tarasov"};
-    private static final String[] names = {"Иван", "Петр", "Артём", "Александр", "Алексей", "Михаил", "Андрей", "Роман", "Сергей"};
-    private static final String[] namesForMail = {"i", "p", "a", "a", "a", "m", "a", "r", "s"};
-    private static final String[] patronymics = {"Иванович", "Петрович", "Виссарионович", "Алексеевич", "Александрович", "Михаилович", "Андрееевич", "Романович", "Сергеевич"};
-    private static final String[] patronymicsForMAil = {"i", "p", "v", "a", "a", "m", "a", "r", "s"};
-    private static final String[] positions = {"инженер", "нахлебник"};
-    private static final String compname = "firmname";
 
     public Person() {
         this.surname = "";
@@ -29,27 +21,7 @@ public class Person {
         this.salary = 0;
     }
 
-    public void addRandom() {
-        int randSurnames = (int) (Math.random() * surnames.length);
-        int randNames = (int) (Math.random() * names.length);
-        int randPatronymics = (int) (Math.random() * patronymics.length);
-        String phoneNumber = "+79";
-        String s = "0123456789";
-        for (int i = 0; i < 10; i++) {
-            phoneNumber = phoneNumber + s.charAt((int) (Math.random() * 10));
-        }
-
-        this.surname = surnames[randSurnames];
-        this.name = names[randNames];
-        this.patronymic = patronymics[randPatronymics];
-        this.position = positions[(int) (Math.random()+0.2)];
-        this.email = surnamesForMail[randSurnames]+"."+namesForMail[randNames]+patronymicsForMAil[randPatronymics]+"@" + compname + ".com";
-        this.telephone = phoneNumber;
-        this.age = 20 + (int) (Math.random() * 45);
-        this.salary = 15000 + (Math.abs((int) (Math.random()*85)) *1000);
-    }
-
-    public void print() {
+        public void print() {
         String years = "";
         switch (this.getAge() % 10) {
              case 1 : years = "год"; break;
--- src/main/java/Lesson5/Persons.java
+++ src/main/java/Lesson5/Persons.java
@@ -5,13 +5,21 @@ public class Persons {
     private int size;
     private final int capacity = 5;
 
+    private static final String[] surnames = {"Иванов", "Петров", "Сидров", "Смирнов", "Кузнецов", "Попов", "Морозов", "Козлов", "Фёдоров", "Тарасов"};
+    private static final String[] surnamesForMail = {"ivanov", "petrov", "sidrov", "smirnov", "kuznetcov", "popov", "morozov", "kozlov", "fedorov", "tarasov"};
+    private static final String[] names = {"Иван", "Петр", "Артём", "Александр", "Алексей", "Михаил", "Андрей", "Роман", "Сергей"};
+    private static final String[] namesForMail = {"i", "p", "a", "a", "a", "m", "a", "r", "s"};
+    private static final String[] patronymics = {"Иванович", "Петрович", "Виссарионович", "Алексеевич", "Александрович", "Михаилович", "Андрееевич", "Романович", "Сергеевич"};
+    private static final String[] patronymicsForMAil = {"i", "p", "v", "a", "a", "m", "a", "r", "s"};
+    private static final String[] positions = {"инженер", "нахлебник"};
+    private static final String compname = "firmname";
+
     public Persons() {
         size = 0;
         data = new Person[capacity];
         for (int i = 0; i < capacity; i++)
             data[i] = new Person();
     }
-
 
     public void printAll() {
         for (int i = 0; i < size; i++) {
@@ -23,8 +31,8 @@ public class Persons {
 
     public void add() {
         if (size < capacity) {
-            data[size].addRandom();
-            this.size++;
+            addRandom();
+            size++;
         }
         else System.out.println("Нет места в справочнике для рандомного сотрудника!\n");
     }
@@ -39,17 +47,36 @@ public class Persons {
             data[size].setTelephone(telephone);
             data[size].setAge(age);
             data[size].setSalary(salary);
-            this.size++;
-
+            size++;
         }
         else System.out.println("Нет места в справочнике для сотрудника!\n");
     }
 
     public void addMax() {
-        for (int i = size; i < capacity; i++) {
-            data[i].addRandom();
-            this.size = capacity;
+        for (int i = size; i <= capacity; i++) {
+            addRandom();
+            size = i;
         }
+    }
+
+    public void addRandom() {
+        int randSurnames = (int) (Math.random() * surnames.length);
+        int randNames = (int) (Math.random() * names.length);
+        int randPatronymics = (int) (Math.random() * patronymics.length);
+        String phoneNumber = "+79";
+        String s = "0123456789";
+        for (int i = 0; i < 10; i++) {
+            phoneNumber = phoneNumber + s.charAt((int) (Math.random() * 10));
+        }
+
+        data[size].setSurname(surnames[randSurnames]);
+        data[size].setName(names[randNames]);
+        data[size].setPatronymic(patronymics[randPatronymics]);
+        data[size].setPosition(positions[(int) (Math.random()+0.2)]);
+        data[size].setEmail(surnamesForMail[randSurnames]+"."+namesForMail[randNames]+patronymicsForMAil[randPatronymics]+"@" + compname + ".com");
+        data[size].setTelephone(phoneNumber);
+        data[size].setAge(20 + (int) (Math.random() * 45));
+        data[size].setSalary(15000 + (Math.abs((int) (Math.random()*85)) *1000));
     }
 
     public void showOlder(int value) {
